7760bb4338da065c4b751dfbb2b7917cee9e0236
Louis <Louis.Medina@flatironschool.com>
2023-02-24 15:56:54 -0400

    create Class method for Pet class

--- 04_OOP_2/lib/pet.py
+++ 04_OOP_2/lib/pet.py
@@ -8,14 +8,24 @@ class Pet:
 # 4. ✅ Define a class attribute (total_pets) and set it to 0
 
     # Class Attribute
+    total_pets = 0
 
     # What happens with our instances when we update a class attribute?
 
-    def __init__(self,name, age, breed, temperament):
+    def __init__(self, name, age, breed, temperament):
         self.name = name
         self.age = age
         self.breed = breed
         self.temperament = temperament
+
+        # Without Class Method
+        # Pet.total_pets += 1
+        
+        # With Class Method
+        Pet.increase_pets()
+        
+        # Treating increase_pets As Instance Method
+        # self.increase_pets()
 
 # ipdb.set_trace()
 
@@ -28,14 +38,20 @@ class Pet:
     # replace Pet.total_pets += 1 in __init__ with increase_pets()
 
     # Instance Method
-    # def print_pet_details(self):
-    #     print(f'''
-    #         name:{self.name}
-    #         age:{self.age}
-    #         breed:{self.breed}
-    #         temperament:{self.temperament}
-    #     ''')
+    def print_pet_details(self):
+        print(f'''
+            name:{self.name}
+            age:{self.age}
+            breed:{self.breed}
+            temperament:{self.temperament}
+        ''')
 
     # Class Method
+    @classmethod # Step 1 => Keyword
+    def increase_pets(cls): # Step 2 => cls Param
+        cls.total_pets += 1
 
-# ipdb.set_trace()
+        # Perform Other Behaviors
+        print("One New Pet Added!")
+
+ipdb.set_trace()
