0f701d115416295dd6bdf1b41356b1202a4dfa39
Louis <Louis.Medina@flatironschool.com>
2023-03-03 13:26:57 -0500

    update starter code for 07 lecture

--- 07-SQLAlchemy-and-Alembic/app/debug.py
+++ 07-SQLAlchemy-and-Alembic/app/debug.py
@@ -1,54 +1,58 @@
 
-# 3.✅ CRUD practice
-# To run the file run `python3 console.py` in the app directory
+# 3. ✅ CRUD Practice
+
+# To run the file, run `python3 debug.py` in the app directory
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models import (Base, Pet)
 
 if __name__ == '__main__':
-    #3.1 ✅ Uncomment bellow to create the engine
+    
+    #3.1 ✅ Uncomment below to create the engine.
     # engine = create_engine('sqlite:///pet_app.db')
     # Base.metadata.create_all(engine)
-    #3.2 ✅ Uncomment bellow to create sessions and bind o the engine
+    
+    #3.2 ✅ Uncomment below to create sessions and bind to the engine.
     # Session = sessionmaker(bind=engine)
     # session = Session()
 
-    #3.3 ✅  -- Creating records
-        # Create a pet and save it to the data base with session.add and session.commit
+    #3.3 ✅ Create
+    
+        # Create a pet and save it to the database with 'session.add' and 'session.commit'
 
-        # Create multiple pets and bulk save them with  session.bulk_save_objects and session.commit
- 
+        # Create multiple pets and bulk save them with  'session.bulk_save_objects' and 'session.commit'
 
-        #session.add(rose)
-        #Note: bulk save will not contain the id
+            # Note: Bulk save will not contain the id
 
-        #verify by checking the db 
+        # Verify results by checking the db 
+
     #3.4 ✅ Read
-        # Get all with session.query
+        
+        # Retrieve all pets with session.query
+
         # Print the pets 
 
-        #Get all of the pet names and print them with session.query
-   
-        #Get all the pet names and print them in order with session.query and order_by
+        # Get all of the pet names and print them with session.query
 
-        #Get the first pet with session.query and first
+        # Get all the pet names and print them in order with session.query and order_by
 
+        # Get the first pet with session.query and first
 
-        #Filter pet by temperament with session.query and filter 
-
+        # Filter pet by temperament with session.query and filter 
 
     #3.5 ✅ Update 
-        # Update the pets name and print the updated pet info
+        
+        # Update the first pet's name and print the updated pet info
+
+        # Update all the pets' temperaments to 'cool' and print the pets 
+
+    #3.6 ✅ Delete
+        
+        # Delete one item by querying the first pet, deleting it and committing
+
+        # Delete all the pets with session.query and .delete
   
-        # Update all the pets temperament to 'cool' and print the pets 
-   
-
-    #3.6 ✅  Delete
-        # Delete one item by querying the first pet, deleting it and committing it
-
-        #delete all the pets with session.query and .delete
-  
-
     # optional Break point for debugging and testing
     import ipdb; ipdb.set_trace()
--- 07-SQLAlchemy-and-Alembic/app/models.py
+++ 07-SQLAlchemy-and-Alembic/app/models.py
@@ -1,9 +1,10 @@
-#1.✅ Build out Model
+#1. ✅ Build out Pet Model
+
 # Import from sqlalchemy: PrimaryKeyConstraint, Column, String, Integer
+
 # Import from sqlalchemy.ext.declarative, declarative_base  
 
-#1.a ✅ Initialize declarative_base and save it to a variable called Base
-
+#1.a ✅ Initialize declarative_base and assign it to a variable called Base
 
 #1.b ✅ Create a class Pet that inherits from Base
 
@@ -11,24 +12,22 @@
     # Add table args for a primary key constraint based off the id
 
     #Create the following columns
-    # id -> type integer
-    # name -> type string
-    # species -> type string
-    # breed -> type string
-    # temperament -> type string
-    # owner_id -> integer 
+        # id -> type integer
+        # name -> type string
+        # species -> type string
+        # breed -> type string
+        # temperament -> type string
+        # owner_id -> type integer 
 
-
+    # Add a __repr__ method that returns a string containing the id, name, species, breed and temperament of our class
     
-    #add a __repr__ method that returns a string containing the id, name, species, breed and temperament of our class
-    
-
 #Note: Nothing further goes in this file.
-# The following will generate a number of folders and files
 
-#2.✅ Migrations 
-# In the app directory run `alembic init migrations`
-# Your directory structure should look like the following 
+# In the the following section, we will generate a number of folders and files
+
+#2. ✅ Migrations 
+# In the app directory, run `alembic init migrations`
+# Your directory structure should look like the following:
 # └── migrations
 #     └── versions
 #     ├── env.py
@@ -39,13 +38,15 @@
 # └── models.py
 
 #2.b Configuration
-    # In alembic.ini, find `sqlalchemy.url`` and set it to `sqlalchemy.url = sqlite:///pet_app.db`
-    # In env.py, find `target_metadata` and add `from models import Base` above it. Next, set target_metadata to `target_metadata = Base.metadata`
+    # In alembic.ini, find `sqlalchemy.url` and set it to `sqlalchemy.url = sqlite:///pet_app.db`
+    # In env.py, find `target_metadata` and add `from models import Base` above it. 
+    # Next, set target_metadata to `target_metadata = Base.metadata`
 
 #2.c ✅ Generate a migration by running `alembic revision --autogenerate -m "Added Pet model"`
+    
     # pet_app.db should have been added to your file structure
 
     # Take the time to review the migration and verify the database with SQLite Explorer or DB Browser
 
-# 3✅ Head to debug.py to test out CRUD actions. 
+# 3. ✅ Head to debug.py to test out CRUD actions
 
